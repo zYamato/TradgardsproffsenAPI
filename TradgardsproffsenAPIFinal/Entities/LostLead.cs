79a856f6fd151cec14d69c84e4bb57075a3b5a39
Yamato <Jesperceriksson@outlook.com>
2021-02-08 09:45:06 +0100

    Changes to LeadJob LostLead And some more Migrations

--- TradgardsproffsenAPIFinal/Entities/LostLead.cs
+++ TradgardsproffsenAPIFinal/Entities/LostLead.cs
@@ -2,16 +2,16 @@
 
 namespace TradgardsproffsenAPI.Entities 
 {
-        public class LostLead 
-        {
-            [Required]
-            [Key]
-            public int Id { get; set; }
-            public Lead Lead { get; set; }
-            public int? LeadId { get; set; }
-            public ValidatedLead ValidatedLead { get; set; }
-            public int? ValidatedLeadId { get; set; }
-            public SentOutLead SentOutLead { get; set; }
-            public int? SentOutLeadId { get; set; }
-        }
+    public class LostLead 
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        public Lead Lead { get; set; }
+        public int? LeadId { get; set; }
+        public ValidatedLead ValidatedLead { get; set; }
+        public int? ValidatedLeadId { get; set; }
+        public SentOutLead SentOutLead { get; set; }
+        public int? SentOutLeadId { get; set; }
+    }
 }
---
+++ TradgardsproffsenAPIFinal/Migrations/20210208081418_ChangedLostLEad.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TradgardsproffsenAPI.Migrations
+{
+    public partial class ChangedLostLEad : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_LeadJobb_JobId",
+                table: "LeadJobb",
+                column: "JobId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LeadJobb_Job_JobId",
+                table: "LeadJobb",
+                column: "JobId",
+                principalTable: "Job",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LeadJobb_Job_JobId",
+                table: "LeadJobb");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LeadJobb_JobId",
+                table: "LeadJobb");
+        }
+    }
+}
